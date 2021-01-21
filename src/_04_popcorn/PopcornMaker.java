b5e4ff3badf255e97c768e9d0d9c039d34c6471a
root <root@359d87c6d416>
2021-01-21 00:45:31 +0000

    Completed Popcorn.

--- src/_04_popcorn/PopcornMaker.java
+++ src/_04_popcorn/PopcornMaker.java
@@ -1,9 +1,20 @@
 package _04_popcorn;
 
+import javax.swing.JOptionPane;
+
 public class PopcornMaker {
 	public static void main(String[] args) {
 		
-		Popcorn pop = new Popcorn("regular");
+		String flavor = JOptionPane.showInputDialog("What flavor do you want?");
+		Popcorn pop = new Popcorn(flavor);
+		Microwave microwave = new Microwave();
+		microwave.putInMicrowave(pop);
+		String time = JOptionPane.showInputDialog("How long do you want to microwave it in minutes?");
+		int t = Integer.parseInt(time);
+		microwave.putInMicrowave(pop);
+		microwave.setTime(t);
+		microwave.startMicrowave();
+		pop.eat();
 		
 	}
 }
---
+++ src/_05_vault/Vault.java
@@ -0,0 +1,13 @@
+package _05_vault;
+
+public class Vault {
+
+	Vault vault = new Vault();
+	int code;
+	void tryCode(int num) {
+
+	}
+	public static void main(String[] args) {
+		
+	}
+}
